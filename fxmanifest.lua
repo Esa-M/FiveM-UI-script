ce33febc9204bf072e0d49a60f7814405b189da0
Ram Prasath S <ram97prasath@gmail.com>
2024-01-19 21:13:58 +0530

    initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name "b4_progressbar"
+description "Progress Bar"
+author "Dark Knight"
+version "0.0.1"
+
+shared_scripts {
+	'shared/*.lua'
+}
+
+client_scripts {
+	'client/*.lua'
+}
+
+server_scripts {
+	'server/*.lua'
+}
